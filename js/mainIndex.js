a1b99a346aee00f47420985a41e9d1a434158de6
AbdelrhmanBadran <AbdelrhmanBadran>
2023-03-04 08:31:23 +0200

    WeatherEdit

--- js/mainIndex.js
+++ js/mainIndex.js
@@ -10,7 +10,6 @@ let data = await result.json();
 // console.log(data.forecast.forecastday)
 DisplayLiveDay(data.location , data.current)
 DisplayNextDays(data.forecast.forecastday)
-console.log('f');
 
 }
 
@@ -26,6 +25,7 @@ let cityName = city.target.value;
 getWeatherData(cityName)
 })
 
+//             0            1            2          3           4            5 
 let days = [ 'Sunday' , 'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday' ];
 
 let months = ["January","February","March" , "April" , "May" , "June" , "July","August","September","October","November","December"];
@@ -35,70 +35,81 @@ let date = new Date();
 let cartoona = 
 `
 
-  <div class="col-md-4 col-sm-12">
-      <div class="main-day text-white">
-          <div class="title hstack bg-dark bg-opacity-50  justify-content-between p-2">
-              <p>${days[date.getDay()]}</p>
-              <p>${date.getDate()} ${months[date.getMonth()]}</p>
-          </div>
-          <div class="weather-caption  mt-1 p-5">
-              <p class = 'text-muted fw-bold '>${cityLocation.name} </p>
-              <div class="icon d-flex justify-content-around">
-              <h1 class='temp '>${cityCurrent.temp_c}<sup>o</sup>C</h1>
+<div class="col-md-4 col-sm-12">
+    <div class="main-day text-white">
+        <div class="title hstack bg-dark bg-opacity-50  justify-content-between p-2">
+            <p>${days[date.getDay()]}</p>
+            <p>${date.getDate()} ${months[date.getMonth()]}</p>
+        </div>
+        <div class="weather-caption  mt-1 p-5">
+            <p class = 'text-muted fw-bold '>${cityLocation.name} </p>
+            <div class="icon d-flex justify-content-around">
+            <h1 class='temp '>${cityCurrent.temp_c}<sup>o</sup>C</h1>
 
-              <img src="https:${cityCurrent.condition.icon}" alt="" class ='icon-waether'>
-              
-              </div>
-              <p class=' text-info' >${cityCurrent.condition.text}</p>
-              <div class=' mt-5 d-flex justify-content-around'>
-              <span>
-              <img src='img/icon-wind.png' width='20px' class='me-1'>
-              ${cityCurrent.wind_kph}kph
-              
-              </span>
-              <span>
-              <img src='img/icon-compass.png' width='20px' class='me-1'>
-              ${cityCurrent.wind_dir}</span>
-              <span>
-              <img src='img/icon-umberella.png' width='20px' class='me-1'>
+            <img src="https:${cityCurrent.condition.icon}" alt="" class ='icon-waether'>
+            
+            </div>
+            <p class=' text-info' >${cityCurrent.condition.text}</p>
+            <div class=' mt-5 d-flex justify-content-around'>
+            <span>
+            <img src='img/icon-wind.png' width='20px' class='me-1'>
+            ${cityCurrent.wind_kph}kph
+            
+            </span>
+            <span>
+            <img src='img/icon-compass.png' width='20px' class='me-1'>
+            ${cityCurrent.wind_dir}</span>
+            <span>
+            <img src='img/icon-umberella.png' width='20px' class='me-1'>
 
-              ${cityCurrent.humidity}%</span>
-              </div>
-          </div>
-          
-      </div>
-  </div>
+            ${cityCurrent.humidity}%</span>
+            </div>
+        </div>
+        
+    </div>
+</div>
 `
 document.getElementById('result').innerHTML = cartoona
 }
 
 function DisplayNextDays(cityForecast){
-// forecastday = data.forecastday[]
 let date = new Date();
 cartoona = ''
 for (let index = 1; index < (cityForecast.length); index++) {
+    let dayWeek =( date.getDay() + index )
+console.log(index);
+if(dayWeek > 6 ){
+    dayWeek = index - 1
+
+}
+// console.log(dayWeek);
+
+// console.log(days[dayWeek]);
+
 cartoona +=
 `
 <div class="col-md-4 col-sm-12">
-      <div class="main-day caption${index} text-white">
-          <div class="title text-center p-2 header${index} ">
-              <p>${days[date.getDay() + index]}</p>
-          </div>
-          <div class=" d-flex flex-column align-items-center justify-content-center mt-5">
-              <img src="https:${cityForecast[index].day.condition.icon}" alt="" class ='icon-waether'>
-              <h1 class=''>${cityForecast[index].day.maxtemp_c}<sup>o</sup>C</h1>
-              <p class=''>${cityForecast[index].day.mintemp_c}<sup>o</sup>C</p>
-              <p class=' text-info' >${cityForecast[index].day.condition.text}</p>
-          </div>
-          
-      </div>
-  </div>
+    <div class="main-day caption${index} text-white">
+        <div class="title text-center p-2 header${index} ">
+            <p>${days[dayWeek]}</p>
+        </div>
+        <div class=" d-flex flex-column align-items-center justify-content-center mt-5">
+            <img src="https:${cityForecast[index].day.condition.icon}" alt="" class ='icon-waether'>
+            <h1 class=''>${cityForecast[index].day.maxtemp_c}<sup>o</sup>C</h1>
+            <p class=''>${cityForecast[index].day.mintemp_c}<sup>o</sup>C</p>
+            <p class=' text-info' >${cityForecast[index].day.condition.text}</p>
+        </div>
+        
+    </div>
+</div>
 
 
 `
 
 
 }
+
 document.getElementById('result').innerHTML += cartoona;
 
 }
+
